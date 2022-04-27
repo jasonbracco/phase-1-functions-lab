23bd43254f0e614ffeb37065918f144198c6c999
Jason Bracco <bracco.jason@gmail.com>
2022-04-27 18:23:01 -0400

    commit tree a0e2b9380d1476e2fac16e0c5af01375b17d99cc

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block){
+    return (block>42)? block-42:42-block
+}
+
+
+function distanceFromHqInFeet(block){
+    return distanceFromHqInBlocks(block) * 264
+}
+
+
+function distanceTravelledInFeet(start, destination){
+    if (start>destination){
+        return ((start-destination)*264)
+    }
+    else{
+        return ((destination-start)*264)
+    }
+}
+
+
+function calculatesFarePrice(start, destination){
+    if (distanceTravelledInFeet(start,destination)<400){
+        return 0
+    }
+    else if (distanceTravelledInFeet(start,destination)>=400 && distanceTravelledInFeet(start,destination)<=2000){
+        return ((distanceTravelledInFeet(start,destination)-400)*.02)
+    }
+    else if (distanceTravelledInFeet(start,destination)>2000 && distanceTravelledInFeet(start,destination)<=2499){
+        return 25
+    }
+    else if (distanceTravelledInFeet(start,destination)>2500){
+        return 'cannot travel that far'
+    }
+
+}
